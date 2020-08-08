8f4436704ef9e97e9287d31b84e2c7288b78eb5a
abeer <abeer.sewak@gmail.com>
2020-08-08 15:02:14 -0500

    added product model

--- app.py
+++ app.py
@@ -6,11 +6,30 @@ import os
 
 # Initialization and setup
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, db.sqlite)
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, db.sqlite)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+
+
+# Product Model
+class Product(db.Model):
+    id = db.Column(db.Intger, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    quantity = db.Column(db.Integer)
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+    def __str__(self):
+        return self.name
 
 
 # Run server
