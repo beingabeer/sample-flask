a624aaa4edfb91853e01bce240d3419fea97b694
abeer <abeer.sewak@gmail.com>
2020-11-29 21:31:28 -0600

    added homepage

--- app.py
+++ app.py
@@ -13,26 +13,33 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+@app.route('/')
+def homepage():
+    return "<h1>This is the homepage</h1>"
+
 
 # Product Model
 class Product(db.Model):
-    id = db.Column(db.Intger, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True)
-    description = db.Column(db.String(200))
+    description = db.Column(db.String(200), nullable=True)
     price = db.Column(db.Float)
-    quantity = db.Column(db.Integer)
+    quantity = db.Column(db.Integer, default=1)
 
-    def __init__(self, name, description, price, quantity):
-        self.name = name
-        self.description = description
-        self.price = price
-        self.quantity = quantity
+    # def __init__(self, name, description, price, quantity):
+    #     self.name = name
+    #     self.description = description
+    #     self.price = price
+    #     self.quantity = quantity
 
-    def __str__(self):
-        return self.name
+    def __repr__(self):
+        return f"Product({self.name})"
 
 
 # Run server
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
 
